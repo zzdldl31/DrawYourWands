8c9c3eeaa5038cff37b6c0b508a83cb8ce43cb01
DESKTOP-6ANRTPD\LINE <bacon8282@naver.com>
2020-10-11 21:36:47 +0900

    레이저와 적 처리

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    LaserHitReceiver laserReceiver;
+    public float hp;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        laserReceiver = GetComponent<LaserHitReceiver>();
+        laserReceiver.OnLaserKeep += (s, e) => Damage(e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp < 0)
+            Destroy(gameObject);
+    }
+
+    public void Damage(PointerEventArgs e)
+    {
+        hp -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/LaserHitReceiver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LaserHitReceiver : MonoBehaviour
+{
+#if true
+    private void Awake()
+    {
+        OnLaserKeep += (s, e) => print("성공");
+    }
+#endif
+
+    public PointerEventHandler OnLaserKeep;
+}
