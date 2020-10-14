a7c254105ac2affcc0da491c4b0e1312805cea9e
DESKTOP-6ANRTPD\LINE <bacon8282@naver.com>
2020-10-14 20:27:00 +0900

    몹 생성기, bacon 씬 정리

---
+++ Assets/Scripts/HandTrail.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandTrail : MonoBehaviour
+{
+    public GameObject tracer;
+    public SteamVR_Behaviour_Pose pose;
+    public SteamVR_Action_Boolean interactWithUI = SteamVR_Input.GetBooleanAction("InteractUI");
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pose == null)
+            pose = this.GetComponent<SteamVR_Behaviour_Pose>();
+        if (pose == null)
+            Debug.LogError("No SteamVR_Behaviour_Pose component found on this object", this);
+
+        if (interactWithUI == null)
+            Debug.LogError("No ui interaction action has been set on this component.", this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactWithUI != null && interactWithUI.GetState(pose.inputSource))
+        {
+            time += Time.deltaTime;
+            if (time > 1)
+            {
+                Instantiate(tracer);
+            }
+        }
+        else time = 0;
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject[] enemyObj;
+    public float spawnperiod;
+    public float spawndist;
+    float peacetime = 0;
+    int mobNum = 0;
+
+    void SpawnMob(int enemyNum)
+    {
+
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Instantiate(enemyObj[enemyNum], spawndist * new Vector3(-Mathf.Cos(angle), 0, Mathf.Sin(angle)),Quaternion.Euler(new Vector3(0, angle*180/Mathf.PI, 0)), null);
+
+        mobNum += 1;
+        switch (mobNum)
+        {
+            case 5:
+            case 15:
+                spawnperiod -= 1;
+                break;
+            case 30:
+            case 35:
+                spawnperiod -= 0.2f;
+                break;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        peacetime += Time.deltaTime;
+
+        if (peacetime > spawnperiod)
+        {
+            peacetime -= spawnperiod;
+            SpawnMob(0);
+        }
+
+
+    }
+}
