eab70eb62d3aedbad5f0e945448b694ab0d1da08
DESKTOP-6ANRTPD\LINE <bacon8282@naver.com>
2020-10-15 15:40:51 +0900

    체력바 및 카메라 앞에 캔버스 띄움

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,14 +7,20 @@ public class Enemy : MonoBehaviour
 {
     CharacterController character;
     LaserHitReceiver laserReceiver;
-    public int hp;
+
+    //public int hp;
+    FloatingCanvas player;
     public float defaultSpeed;
     public float attackRange;
     private float speed;
+    public Healthbar healthbar;
+
+    bool isAttacking = false;
 
     // Start is called before the first frame update
     void Awake()
     {
+        player = GameObject.Find("FloatingCanvas").GetComponent<FloatingCanvas>();
         laserReceiver = GetComponent<LaserHitReceiver>();
         character = GetComponent<CharacterController>();
         laserReceiver.OnLaserKeep += (s, e) => Damage(e);
@@ -25,33 +31,45 @@ public class Enemy : MonoBehaviour
         speed = defaultSpeed;
     }
 
-    void UpdateHealth(int damage)
-    {
-        hp -= damage;
-    }
-
-
     // Update is called once per frame
     void Update()
     {
-        if(delayTimer > 0)
+        if (delayTimer > 0)
             delayTimer -= Time.deltaTime;
 
-        if (hp <= 0)
+        if (healthbar.health <= healthbar.minimumHealth)
+        {
             Destroy(gameObject);
+            player.AddKillCount();
+        }
 
         if (Vector3.Distance(PlayerData.inst.transform.position, transform.position) < attackRange)
-            Attack();
+        {
+            if (!isAttacking)
+            {
+                StartCoroutine("Attack");
+                isAttacking = true;
+            }
+        }
         else
             MoveToPlayer();
     }
 
     public event Action OnAttack;
-
-    private void Attack()
+    IEnumerator Attack()
     {
         OnAttack?.Invoke();
+        yield return new WaitForSeconds(1.15f);
+        while (true)
+        {
+            OnAttack?.Invoke();
+            player.TakeDamage(5);
+            yield return new WaitForSeconds(1.5f);
+        }
     }
+
+
+
 
     private void MoveToPlayer()
     {
@@ -65,7 +83,7 @@ public class Enemy : MonoBehaviour
     {
         if(delayTimer <= 0)
         {
-            UpdateHealth(1);
+            healthbar.TakeDamage(1);
             delayTimer = delay;
         }
     }
---
+++ Assets/Scripts/FloatingCanvas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingCanvas : MonoBehaviour
+{
+    public Transform cam;
+    public Healthbar healthbar;
+    public Text scoreboard;
+
+    int killCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerHP = healthbar.maximumHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.position + cam.forward * 1f;
+        transform.LookAt(cam);
+    }
+
+
+    public void TakeDamage(int dmg)
+    {
+        healthbar.TakeDamage(dmg);
+    }
+
+    public void AddKillCount()
+    {
+        scoreboard.text = "Kills: " + (++killCount).ToString();
+    }
+
+
+
+}
